5bb518e499b284064a7e3fcd3836a534bafc37ae
Pradyumna Rout <pradyumna.rout1433@gmail.com>
2026-01-29 13:33:32 +0530

    Initial Commit

---
+++ SwiftUIBase/SwiftUIBaseApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIBaseApp.swift
+//  SwiftUIBase
+//
+//  Created by hb on 29/01/26.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIBaseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
